ee428d60842e8f4e98f859057eb724381cb49fd8
diegokld30 <dfercalderon@poligran.edu.co>
2023-11-07 20:21:50 -0500

    Se agrego el directorio uploads en public y se indexo ImagenController

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -2,26 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
-
 
 class ImagenController extends Controller
 {
-    public function store(Request $request){
-        //$input = $request->all();
+    //
+    public function store(Request $request)
+    {
         $imagen = $request->file('file');
 
-        $nombreImagen = Str::uuid().".".$imagen->extension();
+        $nombreImagen = Str::uuid() . "." . $imagen->extension();
 
         $imagenServidor = Image::make($imagen);
-        $imagenServidor->fit(1000,1000);
+        $imagenServidor->fit(1000, 1000);
 
-        $imagenPath = public_path('uploads').'/'.$nombreImagen;
-        
+        $imagenPath = public_path('uploads') . '/' . $nombreImagen;
         $imagenServidor->save($imagenPath);
 
-        return response()->json(['imagen' => $nombreImagen]);
+        return response()->json(['imagen' => $nombreImagen ]);
     }
 }
+
